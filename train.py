566dbebfda86ff02d11c9103a3cc5f18e24bdce7
Qinzhe Wang <244611022@csu.edu.cn>
2025-09-25 16:33:19 +0800

    Add files via upload

---
+++ train.py
@@ -0,0 +1,82 @@
+import argparse
+import importlib
+from utils import set_gpu, set_seed
+
+def get_command_line_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-project', type=str, default="ConCM")
+    parser.add_argument('-dataset', type=str, default="cifar100",choices=['mini_imagenet', 'cub200', 'cifar100'])
+    parser.add_argument('-dataroot', type=str, default="your_root")
+    parser.add_argument('-pretrain_path', type=str, default='./params')
+    parser.add_argument('-save_path', type=str, default='./checkpoints')
+
+    parser.add_argument('-backbone', type=str, default="resnet12",choices=['resnet18','resnet12'])
+    parser.add_argument('-num_workers', type=int, default=8)
+
+    parser.add_argument('-pretrain_batch', type=int, default=64)
+    parser.add_argument('-pretrain_lr', type=float, default=0.1)
+    parser.add_argument('-pretrain_epochs', type=int, default=100)
+    parser.add_argument('-pretrain_warmup_epochs', type=int, default=0)
+    parser.add_argument('-pretrain_accumulation_steps', type=int, default=1)
+
+    parser.add_argument('-protonet_meta_mode', type=str, default="epoch",choices=["epoch", "task"])
+    parser.add_argument('-protonet_shot', type=int, default=1)
+    parser.add_argument('-protonet_lr', type=float, default=1)
+
+    parser.add_argument('-protonet_batch', type=int, default=128)
+    parser.add_argument('-protonet_epochs', type=int, default=200)
+    parser.add_argument('-protonet_warmup_epochs', type=int, default=10)
+
+    parser.add_argument('-protonet_task_num', type=int, default=1000)
+
+    parser.add_argument('-align_batch', type=int, default=32)
+    parser.add_argument('-align_lr', type=float, default=0.01)
+    parser.add_argument('-align_epochs', type=int, default=100)
+    parser.add_argument('-align_warmup_epochs', type=int, default=0)
+
+    parser.add_argument('-increment_batch', type=int, default=32)
+    parser.add_argument('-increment_lr', type=float, default=0.01)
+    parser.add_argument('-increment_epochs', type=int, default=20)
+    parser.add_argument('-increment_warmup_epochs', type=int, default=0)
+    parser.add_argument('-increment_accumulation_steps', type=int, default=1)
+    parser.add_argument('-num_enhance', type=int, default=10)
+
+
+    parser.add_argument('-test_batch', type=int, default=100)
+    parser.add_argument('-sample_num_base', type=int, default=100)
+    parser.add_argument('-sample_num_novel', type=int, default=50)
+    parser.add_argument('-replay_num', type=int, default=-1)
+
+    parser.add_argument('-novel_weight', type=float, default=2)
+    parser.add_argument('-base_weight', type=float, default=0.05)
+    parser.add_argument('-base_weight_half', type=float, default=0.5)
+
+    parser.add_argument('-calibration_alpha', type=float, default=0.6)
+    parser.add_argument('-calibration_gamma', type=float, default=0.6)
+    parser.add_argument('-calibration_t', type=float, default=16)
+
+    parser.add_argument('-gpu', type=str, default="0")
+    parser.add_argument('-rand_seed', type=int, default=1)
+    parser.add_argument('-contrastive_loss_mode', type=str, default="only_novel",choices=["only_novel", 'all'])
+    parser.add_argument('-plt_cm', action='store_true', default=0)
+    parser.add_argument('-load_mode', type=str, default="train", choices=['load', 'train'])
+    parser.add_argument('-ablation_mode', type=str, default="ConCM", choices=['frozen','finetune','only_MPC','only_DSM_w_Aug','ConCM','ConCM_wo_AUG','only_DSM_wo_AUG'])
+
+    parser.add_argument('-train_mpc_half', action='store_true', default=0)
+    return parser
+
+if __name__ == '__main__':
+    parser = get_command_line_parser()
+    args = parser.parse_args()
+    args.num_gpu = set_gpu(args)
+    set_seed(args.rand_seed)
+    trainer = importlib.import_module('models.%s.fscil_trainer' % (args.project)).FSCILTrainer(args)
+    trainer.train()
+
+
+
+
+
+
+
+
---
+++ utils.py
@@ -0,0 +1,137 @@
+import os
+import random
+import time
+import numpy as np
+import torch
+import models.ConCM.Network as ConCM_Net
+import matplotlib.pyplot as plt
+import logging
+import torch.nn as nn
+import seaborn as sns
+from sklearn.metrics import confusion_matrix
+import torch.nn.functional as F
+
+def ensure_path(path):
+    if os.path.exists(path):
+        pass
+    else:
+        logging.info('create folder:', path)
+        os.makedirs(path)
+
+def save_list_to_txt(name, input_list):
+    f = open(name, mode='w')
+    for item in input_list:
+        f.write(str(item) + '\n')
+    f.close()
+
+def set_seed(seed):
+    if seed == 0:
+        print(' random seed')
+        torch.backends.cudnn.benchmark = True
+    else:
+        random.seed(seed)
+        np.random.seed(seed)
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+
+class Timer():
+    def __init__(self):
+        self.o = time.time()
+
+    def measure(self, p=1):
+        x = (time.time() - self.o) / p
+        x = int(x)
+        if x >= 3600:
+            return '{:.1f}h'.format(x / 3600)
+        if x >= 60:
+            return '{}m'.format(round(x / 60))
+        return '{}s'.format(x)
+
+def set_gpu(args):
+    gpu_list = [int(x) for x in args.gpu.split(',')]
+    print('use gpu:', gpu_list)
+    os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
+    os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
+    return gpu_list.__len__()
+
+def load_model(args,model_path,session):
+    model = ConCM_Net.ConCM(args)
+    model = nn.DataParallel(model, list(range(args.num_gpu)))
+    model = model.cuda()
+    state_dict = torch.load(model_path,weights_only=True)
+    model.module.load_state_dict(state_dict,strict=False)
+    for i in range(1,session+1):
+        new_fc_name='prototype_classifier.classifiers.'+str(i)+'.weight'
+        novel_rv=state_dict[new_fc_name]
+        new_fc = nn.Linear(novel_rv.shape[1], novel_rv.shape[0], bias=False).cuda()
+        new_fc.weight.data.copy_(novel_rv)
+        model.module.prototype_classifier.classifiers.append(new_fc.cuda())
+    model.module.base_knowledge_acquire()
+    return model
+
+
+
+class Averager():
+
+    def __init__(self):
+        self.n = 0
+        self.v = 0
+
+    def add(self, x):
+        if x is None:   # Skipping addition of empty items
+            return
+        self.v = (self.v * self.n + x) / (self.n + 1)
+        self.n += 1
+
+    def item(self):
+        return self.v
+
+def count_acc(logits, label):
+    pred = torch.argmax(logits, dim=1)
+    if torch.cuda.is_available():      
+        return (pred == label).type(torch.cuda.FloatTensor).mean().item()
+    else:
+        return (pred == label).type(torch.FloatTensor).mean().item()
+
+
+def Confusion_Matrix(true_label_list, predicted_label_list, cmap = "jets"):
+    cm = confusion_matrix(true_label_list, predicted_label_list)
+    plt.figure(figsize=(10, 7))
+    sns.heatmap(cm, annot=False, fmt="d", cmap=cmap, xticklabels=np.unique(true_label_list),
+                yticklabels=np.unique(true_label_list))
+
+    tick_interval = 25
+    xticks = np.arange(0, cm.shape[1], tick_interval)
+    yticks = np.arange(0, cm.shape[0], tick_interval)
+
+    plt.xticks(ticks=xticks, labels=xticks, rotation=0, fontsize=24, fontname='Times New Roman')
+    plt.yticks(ticks=yticks, labels=yticks, fontsize=24, fontname='Times New Roman')
+    plt.xlabel('Predicted Labels', fontsize=26, fontname='Times New Roman')
+    plt.ylabel('True Labels', fontsize=26, fontname='Times New Roman')
+    plt.show()
+
+
+def cal_SMD(prototypes,rv):
+    prototypes = F.normalize(prototypes, p=2, dim=-1)
+    rv = F.normalize(rv, p=2, dim=-1)
+    cos_sim = (prototypes * rv).sum(dim=-1)
+    avg_cos_sim = cos_sim.mean()
+    return avg_cos_sim
+
+def cal_BER(true_label_list,predicted_label_list,base_class):
+    binary_true = (true_label_list >= base_class).astype(int)
+    binary_pred = (predicted_label_list >= base_class).astype(int)
+
+    cm = confusion_matrix(binary_true, binary_pred, labels=[0, 1])
+    TN, FP, FN, TP = cm.ravel()
+
+    fpr = FP / (FP + TN) * 100 if (FP + TN) > 0 else 0.0
+    fnr = FN / (TP + FN) * 100 if (TP + FN) > 0 else 0.0
+    ber=(fpr+fnr)/2
+    return fpr,fnr,ber
+
+
+
+
